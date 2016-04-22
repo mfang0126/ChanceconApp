7939a98f7ee458f8b2db973038c112fe39f67ebf
Freedom <fang.ming@zoho.com>
2016-04-23 11:58:25 +1200

    Redo

---
+++ app/src/main/java/freedom/chancecon/chanceconapp/app/AppController.java
@@ -0,0 +1,45 @@
+package freedom.chancecon.chanceconapp.app;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Freedom on 23/04/16.
+ */
+public class AppController extends Application {
+
+    private static final String TAG = AppController.class.getSimpleName();
+
+    private RequestQueue mRequestQueue;
+
+    private static AppController mInstance;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+
+    private RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+}
